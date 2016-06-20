79800ad8696053b1270b6e2c991622ac00c8261f
Caio Filipini <caiofilipini@gmail.com>
2016-06-20 14:52:20 +0200

    First attempt at presenting `createdAt` in a more friendly format

--- app/models/url.js
+++ app/models/url.js
@@ -1,8 +1,20 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
   shortId: attr('string'),
   createdAt: attr('date'),
-  destination: attr('string')
+  destination: attr('string'),
+  formattedCreatedAt: Ember.computed('createdAt', function() {
+    var createdAt = this.get('createdAt');
+    var time = createdAt.getHours() + ':' + createdAt.getMinutes();
+    var date = createdAt.toLocaleDateString('en-GB', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    });
+
+    return date + ' ' + time;
+  })
 });
